936b4ffa09b47b0f68e027916b03bcf7c06e93b0
Fitz161 <Fitz161@users.noreply.github.com>
2020-12-07 19:50:22 +0800

    Update run.py

--- run.py
+++ run.py
@@ -12,23 +12,15 @@ logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 bot = Flask(__name__)
 message_queue = Queue()
-IS_ACTIVE = True
 
 @bot.route('/api/message',methods=['POST'])
 def message():
     data = request.get_data().decode('utf-8')
     data:dict = json.loads(data)
-    global IS_ACTIVE
     if 'meta_event_type' in data:
         return ''
-    elif data.get('message') == '召唤' and data.get('user_id') in ADMIN_LIST:
-        IS_ACTIVE = True
-    elif not IS_ACTIVE:
-        return ''
-    elif data.get('message') == '删库' and data.get('user_id') in ADMIN_LIST:
+    elif data.get('message') == '/exit' and data.get('user_id') in ADMIN_LIST:
         exit()
-    elif data.get('message') == '去面壁' and data.get('user_id') in ADMIN_LIST:
-        IS_ACTIVE = False
     message_queue.put(data)
     return ''
 
