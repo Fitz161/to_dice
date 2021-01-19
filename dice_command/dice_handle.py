3bc09247793d6f0ee856b8300f5a41a77416971a
Fitz161 <Fitz161@users.noreply.github.com>
2021-01-19 16:01:56 +0800

    添加对大写命令的支持

--- dice_command/dice_handle.py
+++ dice_command/dice_handle.py
@@ -8,13 +8,13 @@ from bot_command.command import send_private_msg, send_public_msg, send_long_msg
 
 def main_handle(message_info):
     try:
-        message = message_info['message'][1:]
+        message:str = message_info['message'][1:]
         group_qq = message_info['group_qq']
         QQ = message_info['sender_qq']
         nickname = message_info['nickname']
-        if message[:3] == 'set':
+        if message[:3].lower() == 'set':
             send_string = set_handle(message_info)
-        elif message[:3] == 'coc':
+        elif message[:3].lower() == 'coc':
             send_string = f'{nickname}的调查员作成:'
             trans_tab = str.maketrans(',', ' ', " '{}")
             try:
@@ -24,41 +24,43 @@ def main_handle(message_info):
                 send_string = send_string[:-1]
             except:
                 send_string += str(get_coc_card()[0]).translate(trans_tab)
-        elif message[:2] == 'dk':
+        elif message[:2].lower() == 'dk':
             send_string = sign_in(message_info)
-        elif message[:2] == 'rc' or message[:2] == 'ra':
+        elif message[:2].lower() == 'rc' or message[:2] == 'ra':
             send_string = random_check(message_info)
-        elif message[:5] == 'rules':
+        elif message[:5].lower() == 'rules':
             send_string = '程序猿正在爆肝开发中'
-        elif message[:1] == 'r':
+        elif message[:1].lower() == 'r':
             send_string = r_expression(message_info)
-        elif message[:2] == 'st':
+        elif message[:2].lower() == 'st':
             send_string =st_handle(message_info)
-        elif message[:4] == 'help':
+        elif message[:2].lower() == 'sc':
+            send_string = san_check(message_info)
+        elif message[:4].lower() == 'help':
             send_string = show_command_doc(message_info)
-        elif message == 'jrrp':
+        elif message.lower() == 'jrrp':
             send_string = today_fortune(message_info)
-        elif message[:2] == 'ob':
+        elif message[:2].lower() == 'ob':
             send_string = observer_handle(message_info)
-        elif message[:4] == 'name':
+        elif message[:4].lower() == 'name':
             send_string = random_name(message_info)
-        elif message[:2] == 'nn':
+        elif message[:2].lower() == 'nn':
             send_string = set_name(message_info)
-        elif message[:4] == 'send':
+        elif message[:4].lower() == 'send':
             send_string = dot_send_msg(message_info)
-        elif message[:6] == 'phasor':
+        elif message[:6].lower() == 'phasor':
             send_string = calculate_phasor(message_info)
         else:
             send_string = None
         #只有群消息才会触发的命令
         if message_info['is_group']:
             from bot_command.event_handle import get_group_admin, leave_group
-            if message == 'leave' or message == 'dismiss':
+            if message.lower() == 'leave' or message.lower() == 'dismiss':
                 if QQ in get_group_admin(message_info):
                     leave_group(message_info)
                 else:
                     send_string = '请让管理员发送该命令'
-            elif message == 'bot off':
+            elif message.lower() == 'bot off':
                 if not message_info['sender_qq'] in get_group_admin(message_info):
                     send_string = BOT_NAME + '只聆听管理员的召唤哦'
                 else:
